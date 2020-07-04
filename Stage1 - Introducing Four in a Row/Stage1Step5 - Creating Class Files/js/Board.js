a73e7c2460ddb12fa365fea5b6096726d3298ba9
Marina8989 <marina.mill209@gmail.com>
2020-07-03 18:58:44 -0700

    keep adding the logic to the game

--- Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Board.js
+++ Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Board.js
@@ -1,1 +1,27 @@
-class Borad{}
+class Borad{
+    constructor() {
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = this.createsSpaces();
+    }
+
+
+/** 
+ * Generates 2D array of spaces.
+ * @return  {array} An array of spaces objects
+*/
+ createsSpaces() {
+    const spaces = [];
+     for(let x = 0; x < this.colums ; x++) {
+        const column = [];
+
+        for(let y = 0; y < this.rows; y++) {
+           const space = new Space(x, y);
+           column.push(space);
+        }
+        spaces.push(column);
+     }
+     return spaces;
+ }
+
+}
